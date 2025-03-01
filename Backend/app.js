02b609a5e623f70b698cb2c5179a8b5c54f6134a
FabioMijango <00156022@uca.edu.sv>
2025-03-01 17:26:43 -0600

    Backend Agregado

---
+++ Backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');  
+const cors = require('cors');  
+const connectDB = require('./config/db');  
+const authRoutes = require('./routes/auth');  
+const userRoutes = require('./routes/user');  
+const jobApplicationRoutes = require('./routes/jobApplication');  
+const applyRoutes = require('./routes/apply');  
+const cvRoutes = require('./routes/cv.js');  
+const feedbackRoutes = require('./routes/feedback');  
+const jobOfferRoutes = require('./routes/jobOffer');
+const morgan = require('morgan');
+
+const app = express();  
+connectDB();  
+
+// Configurar CORS  
+app.use(cors({  
+    origin: '*',  
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],  
+    allowedHeaders: ['Content-Type', 'Authorization']  
+}));  
+
+app.use(morgan('dev'));
+app.use(express.json());  
+app.use('/api/auth', authRoutes);  
+app.use('/api/user', userRoutes);  
+app.use('/api/job-application', jobApplicationRoutes);  
+app.use('/api/apply', applyRoutes);  
+app.use('/api/cv', cvRoutes);  
+app.use('/api/feedback', feedbackRoutes);  
+app.use('/api/job-offers', jobOfferRoutes);  
+ 
+
+const PORT = process.env.PORT || 5000;  
+app.listen(PORT, () => {  
+    console.log(`Server running on port ${PORT}`);  
+});
